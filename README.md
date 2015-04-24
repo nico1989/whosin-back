a12b554d7d27ea30e71285175c4f7f29110785ae
lacroute <mc.flurry@hotmail.fr>
2015-04-24 12:59:47 +0200

    Loopback server with 3 moments sample data stored in a mongo server

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ Requirements
 * node v0.12.2
 * npm 2.7.4
 * python 2.7.8
+* register to Strongloop : https://strongloop.com/register/
 
 Commands
 * npm install
-* slc run
+* node .
---
+++ server/boot/create-sample-data.js
@@ -0,0 +1,15 @@
+module.exports = function(app) {
+  // app.dataSources.mongoDs.automigrate('Moment', function(err) {
+  //   if (err) throw err;
+
+  //   app.models.Moment.create([
+  //     {what: 'Drink a beer ?', when: '10:00 AM'},
+  //     {what: 'Eat sushis ?', when: '11:00 PM'},
+  //     {what: 'Suck a dick ?', when: '12:00 AM'},
+  //   ], function(err, moments) {
+  //     if (err) throw err;
+
+  //     console.log('Models created: \n', moments);
+  //   });
+  // });
+};
