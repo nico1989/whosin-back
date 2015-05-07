2f46036d671a5f9f88ec857ba5e0aade4d09b444
lacroute <mc.flurry@hotmail.fr>
2015-05-07 10:51:30 +0200

    Member inherit from base User

--- common/models/moment.js
+++ common/models/moment.js
@@ -1,3 +1,25 @@
 module.exports = function(Moment) {
 
+  Moment.status = function(cb) {
+    console.log('running status');
+    cb(null, '  Executed');
+  };
+
+  Moment.remoteMethod(
+    'status',
+    {
+      http: {path: '/status', verb: 'get'},
+      returns: {arg: 'status', type: 'string'}
+    }
+  );
+
+  Moment.afterRemote('status', function(ctx, userAffected, next){
+    console.log('AFTER REMOTE ***');
+    next();
+  });
+
+  Moment.beforeRemote('status', function(ctx, userAffected, next){
+    console.log('BEFORE REMOTE ***');
+    next();
+  });
 };
--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -1,5 +1,25 @@
 module.exports = function(app) {
 
+  // Create fake Member into the database
+  app.dataSources.db.automigrate('Member', function(err) {
+    if(err) throw err;
+
+    var Member = app.models.Member;
+    Member.create([
+      {email: 'foo@bar.com', password: 'bar'}
+      ], function(err, members){
+        console.log('\n');
+        console.log('id[' + members[0].id + '] ' + members[0].email, ' created');
+
+        // Login fake Member
+        Member.login({email: 'foo@bar.com', password: 'bar'}, function(err, accessToken){
+          if(err) throw err;
+          console.log("accessToken = ", accessToken.id);
+      });
+    });
+  });
+
+
   // Create Moments into the database
   app.dataSources.db.automigrate('Moment', function(err) {
     if(err) throw err;
@@ -7,26 +27,20 @@ module.exports = function(app) {
     var now = Date.now();
     var hour = 14400000;
 
-    app.models.Moment.create([
+    var Moment = app.models.Moment;
+    Moment.create([
       {what: 'Drink a beer ?', when: now + 1 * hour},
       {what: 'Suck my dick ?', when: now + 3 * hour},
       {what: 'Eat my pussy ?', when: now + 6 * hour},
     ], function(err, moments) {
       if (err) throw err;
 
-      console.log('Models created: \n', moments);
+      // console.log('Models created: \n', moments);
+      console.log('Models created \n');
+
+      Moment.status(function(something, data){
+        console.log(data);
+      });
     });
   });
-
-  // Create fake User
-  var User = app.models.User;
-  User.create({email: 'foo@bar.com', password: 'bar'}, function(err, user){
-    console.log(user);
-  });
-
-  // Login fake User
-  // User.login({email: 'foo@bar.com', password: 'bar'}, function(err, accessToken){
-  //   if(err) throw err;
-  //   console.log(accessToken);
-  // });
 };
