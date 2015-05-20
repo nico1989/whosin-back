87f9d21135d0583133dc946dd1986f8aac2a23f4
lacroute <mc.flurry@hotmail.fr>
2015-05-20 12:08:44 +0200

    Fix related model

--- common/models/member.js
+++ common/models/member.js
@@ -1,6 +1,3 @@
 module.exports = function(Member) {
-  Member.afterRemote('login', function(ctx, userAffected, next){
 
-    ctx.res.redirect('/?id=' + userAffected.userId + '&token=' + userAffected.id);
-  });
 };
